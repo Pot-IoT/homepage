924fb2426674aeedbcb4194bdfbb305d0b911fa3
Boyao SONG <boyao.song@pot-iot.com>
2021-05-01 20:27:07 +0800

    Added documentation and modify some other stuffs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './compoents/Navbar/Navbar';
 import Home from './pages/HomePage/Home';
-import Documentation from './pages/Documentation';
+import Documentation from './pages/Documentation/Documentation';
 import Pricing from './pages/Pricing/Pricing';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Footer from './compoents/Footer/Footer';
--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -26,8 +26,8 @@ function Pricing() {
     return (
         <div className='pricing-page-root'>
             <div className='pricing-top-section' id='pricing-top-section'>
-                <h3 class="heading color-neutral600">Pricing</h3>
-                <div class="text-top color-neutral600">Pot-Iot Platform is free to use for <h2> 3 MONTHS </h2> The pricing of Basic plan is shown as below, and if you want more capacity or even a customized feature, please contact us to know more details: contact_us@pot-iot.com.</div>
+                <h3 class="heading color-black">Pricing</h3>
+                <div class="text-top color-black">Pot-Iot Platform is free to use for <h2> 3 MONTHS </h2> The pricing of Basic plan is shown as below, and if you want more capacity or even a customized feature, please contact us to know more details: contact_us@pot-iot.com.</div>
                 <br />
                 <Link to='/signup'>
                     <Button buttonSize='btn--wide' buttonColor='blue'> Try for free </Button>
@@ -37,9 +37,9 @@ function Pricing() {
                 <Grid container spacing={2} className={classes.grid}>
                     <Grid item xs={12} md={6}>
                         <Paper className={classes.paper} style={{ textAlign: 'left', paddingLeft: "50px", paddingRight: "50px" }}>
-                            <h1 class='color-neutral600' style={{ fontSize: '24px' }}>Basic</h1>
+                            <h1 class='color-black' style={{ fontSize: '24px' }}>Basic</h1>
                             <h1 style={{ color: 'black', width: '100%', fontSize: '48px', padding: '1rem' }}>$100/month</h1>
-                            <div class='color-neutral600' style={{ alignContent: 'flex-start', width: '100%', fontSize: '16px' }}>
+                            <div class='color-black' style={{ alignContent: 'flex-start', width: '100%', fontSize: '16px' }}>
                                 <h3> - 10 connected devices</h3>
                                 <h3> - Accessable for all features </h3>
                                 <h3> - 50 GB data storaging </h3>
@@ -58,7 +58,6 @@ function Pricing() {
                         </Paper>
                     </Grid>
                 </Grid>
-
             </div>
         </div>
     )
